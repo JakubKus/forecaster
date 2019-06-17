992faa325e9aff3a83555fba4561f5d1f984168b
Jakub <kubakus97@o2.pl>
2019-06-17 22:52:02 +0200

    added Search view model with animations

---
+++ Views/SearchView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Forecaster.Views
+{
+    /// <summary>
+    /// Interaction logic for SearchView.xaml
+    /// </summary>
+    public partial class SearchView : UserControl
+    {
+        public SearchView()
+        {
+            InitializeComponent();
+        }
+
+        private void View_Loaded(object sender, RoutedEventArgs e)
+        {
+            Duration animationDuration = new Duration(TimeSpan.FromMilliseconds(600));
+
+            DoubleAnimation widthAnimation = new DoubleAnimation(300, animationDuration);
+            SearchCityBar.BeginAnimation(WidthProperty, widthAnimation);
+
+            DoubleAnimation opacityAnimation = new DoubleAnimation(1, animationDuration);
+            SearchButton.BeginAnimation(OpacityProperty, opacityAnimation);
+
+            DoubleAnimation fontSizeAnimation = new DoubleAnimation(18, animationDuration);
+            SearchButton.BeginAnimation(FontSizeProperty, fontSizeAnimation);
+        }
+    }
+}
